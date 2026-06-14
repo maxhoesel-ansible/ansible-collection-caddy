3f8c149894d5dcfc2d1ce9d439890636c072153c
Max Hösel <git@maxhoesel.de>
2026-06-14 16:23:30 +0200

    bump Ansible to 2.20/21, drop Debian 11 + Python 3.8 (#254)
    support Ansible 2.20/21, drop Debian 11 + Python 3.8

--- roles/caddy_server/README.md
+++ roles/caddy_server/README.md
@@ -9,9 +9,9 @@ Alternatively, you can also configure caddy with a Caddyfile by passing it to th
 ## Requirements
 
 - The following distributions are currently supported and tested:
-  - Ubuntu: 22.04 LTS, 24.04 LTS
+  - Ubuntu: 22.04 LTS, 24.04 LTS, 26.04 LTS
   - Debian: 11, 12, 13
-  - RockyLinux: 9
+  - RockyLinux: 9, 10
 - The following distributions are supported on a best-effort basis (should work but are not tested in CI):
   - Arch Linux
 - Supported architectures: Anything supported by upstream caddy should work
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
-from dataclasses import dataclass
 import importlib
 import importlib.metadata
 import os
-from pathlib import Path
 import subprocess
 import sys
+from dataclasses import dataclass
+from pathlib import Path
 
-from packaging import version
 import pytest
 import yaml
+from packaging import version
 
-NODE_PYTHON_DEFAULT_VERSION = "3.8"
+NODE_PYTHON_DEFAULT_VERSION = "3.9"
 
 with open("galaxy.yml", encoding="utf-8") as f:
     GALAXY_YML = yaml.safe_load(f)
@@ -22,25 +22,38 @@ class TestVersions:
     ansible_version: str
 
 
-class CollectionTestEnv():
+class CollectionTestEnv:
     # pylint: disable=redefined-outer-name
-    def __init__(self, request, test_versions: TestVersions, tmp_path_factory: pytest.TempPathFactory) -> None:
+    def __init__(
+        self,
+        request,
+        test_versions: TestVersions,
+        tmp_path_factory: pytest.TempPathFactory,
+    ) -> None:
         # Sanity check against modifying user/system packages.
         # We need to manually pip-install a version of ansible-core to test with, lets make sure
         # we don't hose the users personal install.
         in_venv = sys.prefix != sys.base_prefix
         if not in_venv and not request.config.getoption("--ci"):
-            raise ValueError("Aborting ansible-core install for tests because we are not inside a virtualenv. "
-                             "Please use ./scripts/setup.sh to setup a virtualenv, then activate it.")
+            raise ValueError(
+                "Aborting ansible-core install for tests because we are not inside a virtualenv. "
+                "Please use ./scripts/setup.sh to setup a virtualenv, then activate it."
+            )
         # install the specified version of ansible-core into our venv
-        subprocess.run(["pip", "install", f"ansible-core~={test_versions.ansible_version}.0"], check=True)
+        subprocess.run(
+            ["pip", "install", f"ansible-core~={test_versions.ansible_version}.0"],
+            check=True,
+        )
 
         # Build collection into isolated install path
         build_path: Path = tmp_path_factory.mktemp("build")
-        collection_tar = build_path / f"{GALAXY_YML['namespace']}-{GALAXY_YML['name']}-{GALAXY_YML['version']}.tar.gz"
+        collection_tar = (
+            build_path
+            / f"{GALAXY_YML['namespace']}-{GALAXY_YML['name']}-{GALAXY_YML['version']}.tar.gz"
+        )
         subprocess.run(
             ["ansible-galaxy", "collection", "build", "--output-path", build_path],
-            check=True
+            check=True,
         )
 
         # Now install the collection into our temporary structure
@@ -49,13 +62,25 @@ class CollectionTestEnv():
             "ANSIBLE_COLLECTIONS_PATH": self._collections_path,
         }
         subprocess.run(
-            ["ansible-galaxy", "collection", "install", "-p", self._collections_path, collection_tar],
-            check=True, env={**os.environ.copy(), **self._env}
+            [
+                "ansible-galaxy",
+                "collection",
+                "install",
+                "-p",
+                self._collections_path,
+                collection_tar,
+            ],
+            check=True,
+            env={**os.environ.copy(), **self._env},
         )
 
         # in case subprocesses need to override the directory, such as for molecule tests
-        self.cwd: Path = (self._collections_path / "ansible_collections" /
-                          GALAXY_YML["namespace"] / GALAXY_YML["name"]).resolve()
+        self.cwd: Path = (
+            self._collections_path
+            / "ansible_collections"
+            / GALAXY_YML["namespace"]
+            / GALAXY_YML["name"]
+        ).resolve()
 
     def run(self, *args, **kwargs):
         # merge env
@@ -65,7 +90,9 @@ class CollectionTestEnv():
             kwargs["env"] = {**os.environ.copy(), **self._env}
 
         if "cwd" not in kwargs:
-            kwargs["cwd"] = self.cwd  # let subclasses overwrite the path, they can get it from self.cwd
+            kwargs["cwd"] = (
+                self.cwd
+            )  # let subclasses overwrite the path, they can get it from self.cwd
         if "check" not in kwargs:
             kwargs["check"] = True  # check by default
         # pylint: disable=subprocess-run-check
@@ -82,7 +109,7 @@ def collection_test_env(request, test_versions, tmp_path_factory) -> CollectionT
 def test_versions(request) -> TestVersions:
     return TestVersions(
         request.config.getoption("--node-python-version"),
-        request.config.getoption("--ansible-version")
+        request.config.getoption("--ansible-version"),
     )
 
 
@@ -92,11 +119,23 @@ def get_ansible_version():
 
 
 def pytest_addoption(parser):
-    parser.addoption("--ci", action="store_true", default=False,
-                     help="Allow unsafe actions such as installing ansible-core directly in CI environments")
-    parser.addoption("--ansible-version", action="store", default=get_ansible_version(),
-                     help="Version of ansible to use for tests, in format 'x.yy', such as '2.15'."
-                     "Default: no constraint (latest/installed)")
-    parser.addoption("--node-python-version", action="store", default=NODE_PYTHON_DEFAULT_VERSION,
-                     help="Python version to test Ansible modules with, "
-                     f"in the format '3.x'. Default: '{NODE_PYTHON_DEFAULT_VERSION}'")
+    parser.addoption(
+        "--ci",
+        action="store_true",
+        default=False,
+        help="Allow unsafe actions such as installing ansible-core directly in CI environments",
+    )
+    parser.addoption(
+        "--ansible-version",
+        action="store",
+        default=get_ansible_version(),
+        help="Version of ansible to use for tests, in format 'x.yy', such as '2.15'."
+        "Default: no constraint (latest/installed)",
+    )
+    parser.addoption(
+        "--node-python-version",
+        action="store",
+        default=NODE_PYTHON_DEFAULT_VERSION,
+        help="Python version to test Ansible modules with, "
+        f"in the format '3.x'. Default: '{NODE_PYTHON_DEFAULT_VERSION}'",
+    )
